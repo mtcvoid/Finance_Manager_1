9edb77f81f460e35e3f415d2e586f32e7f602294
mtcvoid <Mclouse016@Gmail.com>
2024-09-08 07:19:40 -0400

    renamed class to account from new_account

--- accounts/account.py
+++ accounts/account.py
@@ -61,64 +61,13 @@ Savings Balance: {savings_balance}
         :param trans_history:
         :return:
         """
-        return {'User ID': self._user_id, 'Account Holder Name': self.holder_name, 'User name': self.user_name,
-                'User password': self._user_password, 'Transaction History': trans_history,
-                'Checking Balance': checking_balance, 'Savings Balance': savings_balance,
-                'Current Budget Warnings': self.current_budget_warnings}
+        return {'User_ID': self._user_id, 'Account_Holder_Name': self.holder_name, 'User_name': self.user_name,
+                'User_password': self._user_password, 'Transaction_History': trans_history,
+                'Checking_Balance': checking_balance, 'Savings_Balance': savings_balance,
+                'Current_Budget_Warnings': self.current_budget_warnings}
 
 
-def create_new_account():
-    while True:
-        print(f"""
-                   ####Finance Manager####
-                *****************************
-                      Account Creation
-                *****************************
-The following information will be required for creating a new account. 
-        Please review all information for accuracy. 
-                            **
-       - Account username and password for logging into account.
-       - Account holder full name.
-       - A name for the Account. (Ex: John's Investment account)
-            L All account's come with a checking and a savings
-       - Initial fund's
-                            **
-""")
-        while True:
-            y_n = input('Create a new account? (Y)es, (N)o')
-            if y_n.lower() == 'y':
-                user_name = new_account_getter("Username")
-                new_password = new_account_getter('Password')
-                holder_name = new_account_getter("Account holder name")
-                account_name = new_account_getter('Name for the account')
-                break
 
-        account = Account(user_name, new_password, holder_name, account_name)
-        print('Account Created')
-        return account
-
-
-def new_account_getter(getter_type: str):
-    routine_running = True
-    while routine_running:
-        getter = input(f'Please enter a {getter_type}: ')
-        y_n = input(f'Is this correct: {getter}. (Y)es, (N)o')
-        if y_n.lower() == 'y':
-            return getter
-        elif y_n.lower() == 'n':
-            print('Please try again.')
-            continue
-        else:
-            print('Please Enter a valid choice.')
-            o_t = input("""
-(1)Try again
-(2)Exit to main Interface
-                               """)
-            if o_t == '1':
-                continue
-            else:
-                routine_running = False
-                # main_menu() # make this
 
 # def budget_warnings(self, amount):
 #   pass  # set up 4 tiers. with warnings so you know when you go over budget.
