dfe58685f1bd3eea4c1ddb0a3592adac80d8b0df
mtcvoid <Mclouse016@Gmail.com>
2024-09-08 02:09:50 -0400

    updated docstrings updated get_account_details to display transactions correctly

--- accounts/new_account.py
+++ accounts/new_account.py
@@ -9,6 +9,13 @@ class Account:
 
     def __init__(self, user_name: str, new_password: str,
                  holder_name: str = "", account_name: str = None):
+        """
+        Takes in username, password, account holder name, and name of the account.
+        :param user_name:
+        :param new_password:
+        :param holder_name:
+        :param account_name:
+        """
         self._account_name = account_name
         self.holder_name = holder_name
         self._user_id = random.randint(1, 31000)  # this will be used as an ID . All objects will be named account?
@@ -29,13 +36,33 @@ Checking Balance: {checking_balance}
 Savings Balance: {savings_balance}
 ********************************""")
 
-    def get_account_details(self,checking_balance, savings_balance):
+    def get_account_details(self,checking_balance, savings_balance, trans_history):
         """
-        Returns a dictionary value of account details for admin purposes. Use this when
-        manipulating and moving data. This will have more info than the view_account_details
+        Returns a dictionary containing the detailed account information for administrative purposes.
+
+        This method provides a comprehensive view of the account details, including the user ID,
+        account holder name, username, password, transaction history, checking and savings balances,
+        and any current budget warnings. It is intended for backend operations such as data manipulation
+        or retrieval by administrators.
+
+        Parameters:
+        -----------
+        checking_balance : float
+            The current balance of the checking account.
+
+        savings_balance : float
+            The current balance of the savings account.
+
+        trans_history : list
+            A list of the transaction history associated with the account.
+
+        :param checking_balance:
+        :param savings_balance:
+        :param trans_history:
+        :return:
         """
         return {'User ID': self._user_id, 'Account Holder Name': self.holder_name, 'User name': self.user_name,
-                'User password': self._user_password, 'Transaction History': self.transactions,
+                'User password': self._user_password, 'Transaction History': trans_history,
                 'Checking Balance': checking_balance, 'Savings Balance': savings_balance,
                 'Current Budget Warnings': self.current_budget_warnings}
 
