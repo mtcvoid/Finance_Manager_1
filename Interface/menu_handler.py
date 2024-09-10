57684a90c868b16b7c65e8ddd73478288719b02e
mtcvoid <Mclouse016@Gmail.com>
2024-09-10 03:49:44 -0400

    updated logic to address separation of concerns

--- Interface/menu_handler.py
+++ Interface/menu_handler.py
@@ -1,6 +1,6 @@
-
 from Interface.user_interaction import *
-from accounts.account_handler import *
+
+
 
 def menu_header(header_title):
     print(f"""
@@ -19,48 +19,58 @@ def menu_choices(choices_and_functions):
 
 def main_menu():
     header = 'Main Menu'
-    choices_and_funcs = [('LOG IN', log_in), ('View options', main_menu_options)]
+    choices_and_funcs = [('View options', main_menu_options)]
     menu_header(header)
     menu_choices(choices_and_funcs)
-    user_choice = get_user_choice(choices_and_funcs)
-    print(user_choice)
+    get_user_choice(choices_and_funcs)
 
 
 def main_menu_options():
-    menu_header = 'Account Menu'
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
 def account_main_menu():
-    menu_header = 'Account Menu'
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
 def account_options_menu():
-    menu_header = 'Account Options '
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
 def admin_options_menu():
-    menu_header = 'Admin Options'
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
 def market_watch_menu():
-    menu_header = 'Stock Market Watcher '
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
 def bill_tracking_menu():
-    menu_header = 'Bill Tracking'
-    main_choices_and_funcs = []
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = ''
+    choices_and_funcs = []
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    get_user_choice(choices_and_funcs)
 
 
-main_menu()
 
