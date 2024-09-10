bac0ed01e52273d6b59638660d1959059bc53c9b
mtcvoid <Mclouse016@Gmail.com>
2024-09-10 03:26:07 -0400

    functions moved from menu_builder.py to menu_handler

--- Interface/menu_handler.py
+++ Interface/menu_handler.py
@@ -1,11 +1,29 @@
-from Interface.menu_builder import *
+
+from Interface.user_interaction import *
 from accounts.account_handler import *
+
+def menu_header(header_title):
+    print(f"""
+       #####Finance Manager#####
+    ******************************
+             {menu_header}
+    ******************************""")
+
+
+def menu_choices(choices_and_functions):
+    for index, (choice, _) in enumerate(choices_and_functions, start=1):
+        print(f'({index}) {choice}')
+
+    print("******************************")
 
 
 def main_menu():
-    menu_header = 'Main Menu'
-    main_choices_and_funcs = [('LOG IN', log_in()), ('View options', main_menu_options())]
-    menu_builder(menu_header, main_choices_and_funcs)
+    header = 'Main Menu'
+    choices_and_funcs = [('LOG IN', log_in), ('View options', main_menu_options)]
+    menu_header(header)
+    menu_choices(choices_and_funcs)
+    user_choice = get_user_choice(choices_and_funcs)
+    print(user_choice)
 
 
 def main_menu_options():
@@ -42,3 +60,7 @@ def bill_tracking_menu():
     menu_header = 'Bill Tracking'
     main_choices_and_funcs = []
     menu_builder(menu_header, main_choices_and_funcs)
+
+
+main_menu()
+
