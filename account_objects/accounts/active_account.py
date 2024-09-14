14102624fea50d29b08643b60b27fcc2d058f8cd
mtcvoid <Mclouse016@Gmail.com>
2024-09-14 09:12:16 -0400

    created active_account class to handle menu options dealing with current user pulled from database

---
+++ account_objects/accounts/active_account.py
@@ -0,0 +1,34 @@
+import json
+from account_objects.accounting.transactions import Transactions
+
+
+class ActiveAccount:
+    def __init__(self, user_id: int, holder_name: str, user_name: str, user_password,
+                 transactions: list, checking_balance: int, savings_balance: int, c_b_w: int):
+        self.holder_name = holder_name
+        self._user_id = user_id
+        self.user_name = user_name
+        self._user_password = user_password
+        self.transactions = transactions
+        self.current_budget_warnings = c_b_w
+        self.checking_balance = checking_balance
+        self.savings_balance = savings_balance
+
+    def view_account_details(self, checking_balance, savings_balance):
+        return (f"""
+********************************
+Account ID: {self._user_id}
+Account Holder: {self.holder_name}
+Checking Balance: {checking_balance}
+Savings Balance: {savings_balance}
+********************************""")
+
+    def get_account_details(self):
+        tran = Transactions()
+        trans_history = tran.transactions
+
+        return {'User_ID': self._user_id, 'Account_Holder_Name': self.holder_name, 'User_name': self.user_name,
+                'User_password': self._user_password, 'Transaction_History': json.dumps(trans_history),
+                'Checking_Balance': tran.balance['Checking_balance'],
+                'Savings_Balance': tran.balance['Savings_balance'],
+                'Current_Budget_Warnings': self.current_budget_warnings}
