f61f2642483ce4418616330dc5507a2ede2f134d
mtcvoid <Mclouse016@Gmail.com>
2024-09-19 15:58:52 -0400

    added logic for bill handling

--- mods/bill_tracker/bill_tracking_handler.py
+++ mods/bill_tracker/bill_tracking_handler.py
@@ -1,14 +1,21 @@
 """
 logic for bills
 """
+from datetime import datetime
 
 
-class Bills:
+class BillHandler:
 
     def __init__(self):
         self.bills = []
 
     def add_bill(self, bill_name: str, bill_total: float, due_date: str):
+
+        try:
+            due_date = datetime.strptime(due_date, '%Y-%m-%d').date()
+        except:
+            raise ValueError('Due date must be formated  in YYYY-MM-DD')
+
         self.bills.append({'Bill': bill_name, 'Total': bill_total, 'Due Date': due_date})
 
     def remove_bill(self, bill_name):
@@ -18,3 +25,9 @@ class Bills:
 
     def view_all_bills(self):
         return self.bills
+
+    def set_bill_reminder(self):
+        pass
+
+
+
