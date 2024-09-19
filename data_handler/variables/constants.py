ec182d26df3e7f037b2f412f58ad1377d565bfaf
mtcvoid <Mclouse016@Gmail.com>
2024-09-19 02:20:03 -0400

    created to remove hardcoding

---
+++ data_handler/variables/constants.py
@@ -0,0 +1,12 @@
+
+# constants
+
+
+USER_ID = 'user_id'
+ACCOUNT_HOLDER_NAME = 'account_holder_name'
+USER_NAME = 'user_name'
+USER_PASSWORD = 'user_password'
+TRANSACTION_HISTORY = 'transaction_history'
+CHECKING_BALANCE = 'checking_balance'
+SAVINGS_BALANCE = 'savings_balance'
+CURRENT_BUDGET_WARNINGS = 'current_budget_warnings'
