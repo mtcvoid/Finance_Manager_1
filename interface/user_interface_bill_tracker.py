b9fcd083719996810601dd86ecfa0deeccfd80e8
mtcvoid <Mclouse016@Gmail.com>
2024-09-19 16:00:29 -0400

    started user interfacing

--- interface/user_interface_bill_tracker.py
+++ interface/user_interface_bill_tracker.py
@@ -2,6 +2,8 @@
 handles user interaction for bill tracker
 """
 from interface.user_interface_general import *
+from mods.bill_tracker.bill_tracking_handler import BillHandler
+
 
 
 def user_add_bill(active_user_account):
@@ -9,29 +11,33 @@ def user_add_bill(active_user_account):
     You will need the following information: 
     - Name of bill
     - Bill total
-    - Date the bill is due
+    - Date the bill is due (YYY-DD-
     """)
+   
     confirmation = get_user_confirmation("Would you like to add a bill")
     if confirmation:
-        pass
+        bill_handler = BillHandler()
+       # name =
+        #total =
+        #date =
+
+        #confirm
+
+
+        #handly
+        #bill_handler.add_bill(name,total,date)
+
+        #add another bill?
+            #loop back
+
+        #no add
+         #active user menu
+
+
     else:
         pass  # need to add an account menu.
 
-    '''
-    
-    So you will need to figure out the logic behind holding an account in place once you have chosen said account. 
-    if you return to main menu you can log out maybe? set up a log in and log out method maybe?
-    
-    - if you set up a log in method then once the user has logged in they it can set that account to 'Active' and then 
-    that account is what gets pulled for all of the menu options? think about this before you go and do market watch 
-    stuff. 
-    
-    
-    
-    def menu_maker(menu_key):          
-  
-        'Chosen-Account Menu'
-    '''
+
 
 
 def user_remove_bill():
@@ -39,4 +45,10 @@ def user_remove_bill():
 
 
 def user_view_all_bills():
-    pass
+    bill = BillHandler()
+    bill.add_bill('Comcast', 150.35, "2024-10-05")
+
+    for item in bill.bills:
+        for key, value in item.items():
+            print(key, value)
+
