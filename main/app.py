ef7b49af1b1f0632f83421879539217732a60e3c
mtcvoid <Mclouse016@Gmail.com>
2024-09-19 16:01:14 -0400

    added  DatabaseUnpacker so database is created on launch

--- main/app.py
+++ main/app.py
@@ -1,4 +1,8 @@
 from interface.menu_handler import *
+from data_handler.database.data_base_handler import DatabaseUnpacker
+if __name__ == "__main__":
+    # Ensure the database table is created when the application starts
+    db_unpacker = DatabaseUnpacker()
 
-
-main_menu()
+    # Now call your main menu or other application logic
+    main_menu()
