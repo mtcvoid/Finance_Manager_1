cfa2822e4209446e7404b06797dde9617db82610
mtcvoid <Mclouse016@Gmail.com>
2024-09-20 06:28:13 -0400

    added constants for bills

--- data_handler/variables/constants.py
+++ data_handler/variables/constants.py
@@ -1,14 +1,24 @@
-
 # constants
 
-
+# account
 USER_ID = 'user_id'
 ACCOUNT_HOLDER_NAME = 'account_holder_name'
 USER_NAME = 'user_name'
 USER_PASSWORD = 'user_password'
+
+# transactions and accounting
 TRANSACTION_HISTORY = 'transaction_history'
 CHECKING_BALANCE = 'checking_balance'
 SAVINGS_BALANCE = 'savings_balance'
 CURRENT_BUDGET_WARNINGS = 'current_budget_warnings'
-BILLS = 'bills'
 
+# bill handling
+BILLS = 'bills'
+BILL_REMINDERS = 'bill_reminders'
+TOTAL = 'Total'
+DUE_DATE = 'Due Date'
+PAID = 'paid'
+CURRENT_DATE = 'current_date'
+DAYS = 'days'
+YES = 'yes'
+NO = 'no'
