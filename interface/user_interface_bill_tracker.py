5a89016188650b83dbc52bee5be30bfc756c440c
mtcvoid <Mclouse016@Gmail.com>
2024-09-20 06:27:07 -0400

    added logic to bill handler and user interface

--- interface/user_interface_bill_tracker.py
+++ interface/user_interface_bill_tracker.py
@@ -1,46 +1,68 @@
 """
 handles user interaction for bill tracker
 """
-from interface.user_interface_general import *
-from mods.bill_tracker.bill_tracking_handler import BillHandler
+import time
 
+from interface.user_interface_general import *
+from mods.bill_tracker.bill_tracking_handler import *
 
 
 def user_add_bill(active_user_account):
     print("""
-    You will need the following information: 
+    You will need the following information to add a bill to your account: 
     - Name of bill
     - Bill total
-    - Date the bill is due (YYY-DD-
+    - Date the bill is due (YYYY-MM-DD)
     """)
-   
-    confirmation = get_user_confirmation("Would you like to add a bill")
+
+    # Importing DatabaseUnpacker inside the function to avoid potential circular imports.
+
+    confirmation = get_user_confirmation("Would you like to add a bill? (Y)/(N): ")
     if confirmation:
+
+        from data_handler.database.data_base_handler import DatabaseUnpacker
+
+        # objects for handling bill logic and database transfer
         bill_handler = BillHandler()
-       # name =
-        #total =
-        #date =
+        pusher_puller = DatabaseUnpacker()
 
-        #confirm
+        # Get the active user's account details (such as balances, transaction history, etc.)
+        active = active_user_account.get_account_details()
 
+        # Pull the current data of the user from the database, using the user's ID as a key
+        current_data = pusher_puller.pull_from_database(active[USER_ID])
 
-        #handly
-        #bill_handler.add_bill(name,total,date)
+        # Retrieve the current bills list  from the database and set it in the bills object
+        handler = current_data[BILLS]
 
-        #add another bill?
-            #loop back
+        # get users information  and adds new bill to bill object
+        name = input_with_validation('bill name: ')
+        total = input_with_validation('bill total: ')
+        date = input_with_validation('bill due date (YYYY-MM-DD): ')
 
-        #no add
-         #active user menu
+        # adds bill to handler
+        bill_handler.add_bill(name, total, date, current_date=None, paid=None, days=None)
 
+        # bill object gets transferred back to user for push to database
+        for bill in bill_handler.bills:
+            handler.append(bill)
 
+        # Push the updated account data back into the database to save the changes
+        pusher_puller.push_to_database(handler)
+
+        # returns to previous menu
+        from interface.menu_handler import previous_menu
+        print('Returning to Bill Tracking menu')
+        time.sleep(2)
+        previous_menu(active_user_account, 'Bill Tracker')
     else:
-        pass  # need to add an account menu.
+        from interface.menu_handler import previous_menu
+        print('Returning to Bill Tracking menu')
+        time.sleep(2)
+        previous_menu(active_user_account, 'Bill Tracker')
 
 
-
-
-def user_remove_bill():
+def user_remove_bill(active_user_account):
     pass
 
 
@@ -51,4 +73,5 @@ def user_view_all_bills():
     for item in bill.bills:
         for key, value in item.items():
             print(key, value)
-
+def user_set_reminder():
+    pass
--- mods/bill_tracker/bill_tracking_handler.py
+++ mods/bill_tracker/bill_tracking_handler.py
@@ -2,32 +2,35 @@
 logic for bills
 """
 from datetime import datetime
+from data_handler.variables.constants import *
 
 
 class BillHandler:
 
     def __init__(self):
         self.bills = []
+        self.bill_reminders = []
 
-    def add_bill(self, bill_name: str, bill_total: float, due_date: str):
-
-        try:
-            due_date = datetime.strptime(due_date, '%Y-%m-%d').date()
-        except:
-            raise ValueError('Due date must be formated  in YYYY-MM-DD')
-
-        self.bills.append({'Bill': bill_name, 'Total': bill_total, 'Due Date': due_date})
+    def add_bill(self, bill_name: str, bill_total: float, due_date: str, current_date, paid=NO, days=None):
+        self.bills.append({BILLS: bill_name, TOTAL: bill_total, DUE_DATE: due_date, CURRENT_DATE: current_date, PAID: paid, DAYS: days})
 
     def remove_bill(self, bill_name):
         for bill in self.bills:
-            if bill['Bill'] == bill_name:
+            if bill[BILLS] == bill_name:
                 self.bills.remove(bill)
 
-    def view_all_bills(self):
-        return self.bills
+    def get_bill_reminders(self, bill_name):
 
-    def set_bill_reminder(self):
-        pass
+        for bill in self.bills:
+            current_date = datetime.now().date()
+            days = (bill[DUE_DATE] - current_date).days
+            bill[DAYS] = days
+            bill[CURRENT_DATE] = current_date
+            self.bill_reminders.append(bill)
 
+        return self.bill_reminders
 
-
+    def bill_paid(self, bill_name):
+        for bill in self.bills:
+            if bill[BILLS] == bill_name:
+                bill[PAID] = YES
