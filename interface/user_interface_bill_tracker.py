228442a3dd77709d741823b649789b3e4eb4965c
mtcvoid <Mclouse016@Gmail.com>
2024-09-21 03:16:10 -0400

    -added doc strings -replaced 'previous_menu' with menu_maker -added logic so users could see all bills attached to the account and days left until due date.

--- interface/user_interface_bill_tracker.py
+++ interface/user_interface_bill_tracker.py
@@ -8,6 +8,23 @@ from mods.bill_tracker.bill_tracking_handler import *
 
 
 def user_add_bill(active_user_account):
+    """
+    Prompts the user to add a new bill to their account and updates the database with the new bill information.
+
+    The function interacts with the user by collecting information about the bill (such as the name, total amount,
+    and due date) and adds this information to the bill tracker. It then updates the user's account in the database
+    with the new bill.
+
+    Args:
+        active_user_account (object): The active user's account object, which contains their account details.
+
+    Flow:
+        1. Prompts the user for bill details.
+        2. Adds the bill to the user's account through the `BillHandler`.
+        3. Updates the user's account information in the database.
+        4. Returns to the previous menu.
+
+    """
     print("""
     You will need the following information to add a bill to your account: 
     - Name of bill
@@ -53,27 +70,67 @@ def user_add_bill(active_user_account):
         pusher_puller.push_to_database(current_data)
 
         # returns to previous menu
-        from interface.menu_handler import previous_menu
+        from interface.menu_handler import menu_maker
         print('Returning to Bill Tracking menu')
         time.sleep(2)
-        previous_menu(active_user_account, 'Bill Tracker')
+        menu_maker('Bill Tracker', active_user_account)
     else:
-        from interface.menu_handler import previous_menu
+        from interface.menu_handler import menu_maker
         print('Returning to Bill Tracking menu')
         time.sleep(2)
-        previous_menu(active_user_account, 'Bill Tracker')
+        menu_maker('Bill Tracker', active_user_account)
 
 
 def user_remove_bill(active_user_account):
+    """
+    Placeholder function for removing a bill from the user's account.
+
+    Args:
+        active_user_account (object): The active user's account object, which contains their account details.
+
+    Note:
+        This function is currently a placeholder and has no implemented functionality.
+    """
     pass
 
 
-def user_view_all_bills():
-    bill = BillHandler()
-    bill.add_bill('Comcast', 150.35, "2024-10-05")
+def user_get_bill_reminders(active_user_account):
+    """
+    Retrieves and displays bill reminders for the user's account.
 
-    for item in bill.bills:
-        for key, value in item.items():
-            print(key, value)
-def user_set_reminder():
-    pass
+    This function pulls the user's bill data from the database and uses the `BillHandler` class to
+    calculate how many days are left until each bill's due date. The reminders are then displayed
+    to the user.
+
+    Args:
+        active_user_account (object): The active user's account object, which contains their account details.
+
+    Flow:
+        1. Retrieves the user's bill data from the database.
+        2. Uses the `BillHandler` to calculate the days left until each bill's due date.
+        3. Displays the reminders for each bill.
+        4. Returns to the Bill Tracking menu.
+    """
+    from data_handler.database.data_base_handler import DatabaseUnpacker
+    from interface.menu_handler import menu_maker
+
+    # objects for handling bill logic and database transfer
+    bill_handler = BillHandler()
+    pusher_puller = DatabaseUnpacker()
+
+    # Get the active user's account details.
+    active = active_user_account.get_account_details()
+    current_data = pusher_puller.pull_from_database(active[USER_ID])
+
+    # Pulls 'bills' from current data in list form to then be added to the bill handler.
+    data_transfer = current_data[BILLS]
+    for item in data_transfer:
+        bill_handler.bills.append(item)
+
+    # Gets current time and prints out all bill information to user.
+    bill_handler.get_bill_reminders()
+
+    # Return to the previous menu.
+    print('Returning to main menu....')
+    time.sleep(2)
+    menu_maker('Bill Tracker', active_user_account)
