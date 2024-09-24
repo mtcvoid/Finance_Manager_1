4896701dfe210a211d16cdbd8ce772ece0eb628f
mtcvoid <Mclouse016@Gmail.com>
2024-09-24 04:49:10 -0400

    -began building

--- interface/user_interface_budget.py
+++ interface/user_interface_budget.py
@@ -1,21 +1,41 @@
 """
 handles user interaction for budget
 """
+from interface.user_interface_general import input_with_validation, get_user_confirmation
+
+def user_add_remove_expense():
+    print('''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Expenses are categorized under certain categories to better
+    help you  understand where you are spending your money. 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Essential Expenses:
+                -Housing        -Utilities
+                -Groceries      -Transportation
+                -Insurance      -Healthcare
+                -Debt Payments (Mortgages, Credit Cards, etc...)
+
+    Non-Essential Expenses:
+                -Dining Out     -Entertainment
+                -Personal Care  -Clothing
+                -Education      -Hobbies and Leisure
+                -Gifts          -Travel/Vacations
+
+    Savings and Investments:
+                -Emergency Fund
+                -Retirement Savings
+                -Investments
+                -Education Savings
+                -Major Purchases (Home, Vehicle, etc....)''')
 
 
-def user_view_current_budget(active_user_account):
-    print(' view current budget test')
+    run_expenses = get_user_confirmation('Would you like to add an expense? (Y)/(N)')
+
+    if run_expenses:
+        pass
+    else:
+        pass # go back to budget menu
 
 
-def user_create_new_budget(active_user_account):
-    print('create new budget test')
 
-
-def user_remove_budget(active_user_account):
-    print('remove budget test')
-
-
-def user_update_budget(active_user_account):
-    print('user_update_test')
-
-
+    pass
