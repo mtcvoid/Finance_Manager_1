738b0fb57b0b27125ec47e76a6fab5a435545c77
mtcvoid <Mclouse016@Gmail.com>
2024-09-24 04:43:49 -0400

    -began adding more attribute for advanced account creation

--- account_objects/accounts/account.py
+++ account_objects/accounts/account.py
@@ -1,67 +1,44 @@
-import json
-import random
-
 from account_objects.accounting.transactions import Transactions
 from data_handler.variables.constants import *
 
 
 class Account:
-    """
-    Represents a user account with basic details such as user ID, account holder name, and account type.
-
-    This class allows account holders to manage their account details, view account information,
-    and check transactions. It also handles password and transaction history for the account.
-
-    Attributes:
-
-        holder_name (str): The name of the account holder.
-        _user_id (int): A unique user ID (initialized to a static value of 654).
-        user_name (str): The username for the account.
-        _user_password (str): The password associated with the account.
-        transactions (list): A list of transactions associated with the account.
-        current_budget_warnings (int): A counter for budget warnings issued to the account holder.
-    """
-
-    def __init__(self, user_name: str, new_password: str,
-                 holder_name: str = ""):
-        """
-          Initializes the Account object with the user's details.
-
-          Args:
-              user_name (str): The username for the account.
-              new_password (str): The password for the account.
-              holder_name (str, optional): The name of the account holder (default is an empty string).
-
-          """
-
+    def __init__(self, user_id: int, holder_name: str, user_name: str, user_password=None,
+                 transactions=None, checking_balance=0, savings_balance=0,
+                 c_b_w=None, bills=None, bill_reminders=None, monthly_income=None, expenses=None, budgets=None,
+                 current_debt=None, total_debt=None,
+                 assets=None, credit_card_balances_and_limits=None, net_worth=None):
+        if transactions is None:
+            transactions = []
         self.holder_name = holder_name
-        self._user_id = random.randint(1, 31324)  # this will be used as an ID . All objects will be named account?
+        self._user_id = user_id
         self.user_name = user_name
-        self._user_password = new_password
-        self.transactions = []
-        self.current_budget_warnings = 0
-        self.bills = []
-        self.bill_reminders = []
+        self._user_password = user_password
+        self.transactions = transactions
+        self.current_budget_warnings = c_b_w
+        self.checking_balance = checking_balance
+        self.savings_balance = savings_balance
+        self.bills = bills
+        self.bill_reminders = bill_reminders
 
-    def view_account_details(self, checking_balance, savings_balance):
-        """
-        Displays the account details, including the checking and savings balances.
+        # these need added to database push and pull
+        self.monthly_income = monthly_income
+        self.expenses = expenses
+        self.budgets = budgets
+        self.budget_alerts = []
+        self.current_debt = current_debt
+        self.total_debt = total_debt
+        self.assets = assets
+        self.credit_card_balances_and_limits = credit_card_balances_and_limits
+        self.net_worth = net_worth
 
-        Args:
-            checking_balance (float): The current balance of the checking account.
-            savings_balance (float): The current balance of the savings account.
-
-        Returns:
-            str: A formatted string containing the account details, such as account ID, account holder name,
-                 and balances for both checking and savings accounts.
-        """
-        return (f"""
-********************************
-Account ID: {self._user_id}
-Account Holder: {self.holder_name}
-Checking Balance: {checking_balance}
-Savings Balance: {savings_balance}
-********************************""")
+    def get_account_details(self) -> dict:
+        return {USER_ID: self._user_id, ACCOUNT_HOLDER_NAME: self.holder_name, USER_NAME: self.user_name,
+                USER_PASSWORD: self._user_password, TRANSACTION_HISTORY: self.transactions,
+                CHECKING_BALANCE: self.checking_balance,
+                SAVINGS_BALANCE: self.savings_balance,
+                CURRENT_BUDGET_WARNINGS: self.current_budget_warnings,
+                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders}
 
     def get_new_account_details(self):
         """
@@ -89,4 +66,4 @@ Savings Balance: {savings_balance}
                 CHECKING_BALANCE: tran.balance[CHECKING_BALANCE],
                 SAVINGS_BALANCE: tran.balance[SAVINGS_BALANCE],
                 CURRENT_BUDGET_WARNINGS: self.current_budget_warnings,
-                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders }
+                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders}
