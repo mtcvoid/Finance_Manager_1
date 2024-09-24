e3c637363e534915eec1d2af8cda0f7203f212db
mtcvoid <Mclouse016@Gmail.com>
2024-09-24 04:47:33 -0400

    updated

--- data_handler/variables/constants.py
+++ data_handler/variables/constants.py
@@ -22,3 +22,35 @@ CURRENT_DATE = 'current_date'
 DAYS = 'days'
 YES = 'yes'
 NO = 'no'
+
+#debt handler
+ADD = 'add'
+REMOVE = 'remove'
+ESSENTIAL_EXPENSES = 'essential expenses'
+NON_ESSENTIAL_EXPENSES = 'non-essential expenses'
+SAVINGS_AND_INVESTMENTS = 'savings and investments'
+# debt handler essential
+HOUSING = 'housing'
+UTILITIES = 'utilities'
+GROCERIES = 'groceries'
+TRANSPORTATION = 'transportation'
+INSURANCE = 'insurance'
+HEALTHCARE = 'healthcare'
+DEBT_PAYMENTS = 'debt payments'
+# debt handler non essential
+DINING_OUT = 'dining out'
+ENTERTAINMENT = 'entertainment'
+PERSONAL_CARE = 'personal care'
+CLOTHING = 'clothing'
+EDUCATION = 'education'
+HOBBIES_AND_LEISURE = 'hobbies and leisure'
+TRAVEL_VACATIONS = 'travel vacations'
+GIFTS_DONATIONS = 'gifts and donations'
+# debt handler savings and investments
+EMERGENCY_FUND = 'emergency fund'
+RETIREMENT_SAVINGS = 'retirement savings'
+INVESTMENTS = 'investments'
+EDUCATION_SAVINGS = 'education savings'
+MAJOR_PURCHASES = 'major purchases'
+
+
