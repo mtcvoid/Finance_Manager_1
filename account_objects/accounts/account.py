ffac54076e96423bc9341896d7222f067c1c2079
mtcvoid <Mclouse016@Gmail.com>
2024-09-24 23:22:48 -0400

    -updated account class to handle simple and advanced account creation. -updated get_account_details for database transfer

--- account_objects/accounts/account.py
+++ account_objects/accounts/account.py
@@ -3,34 +3,32 @@ from data_handler.variables.constants import *
 
 
 class Account:
-    def __init__(self, user_id: int, holder_name: str, user_name: str, user_password=None,
+    def __init__(self, user_id: int, holder_name: str, user_name: str, user_password: str,
                  transactions=None, checking_balance=0, savings_balance=0,
                  c_b_w=None, bills=None, bill_reminders=None, monthly_income=None, expenses=None, budgets=None,
-                 current_debt=None, total_debt=None,
+                 budget_alerts=None, current_debt=None, total_debt=None,
                  assets=None, credit_card_balances_and_limits=None, net_worth=None):
-        if transactions is None:
-            transactions = []
         self.holder_name = holder_name
         self._user_id = user_id
         self.user_name = user_name
         self._user_password = user_password
-        self.transactions = transactions
-        self.current_budget_warnings = c_b_w
-        self.checking_balance = checking_balance
-        self.savings_balance = savings_balance
-        self.bills = bills
-        self.bill_reminders = bill_reminders
+        self.transactions = transactions or []
+        self.current_budget_warnings = c_b_w or []
+        self.checking_balance = checking_balance or 0
+        self.savings_balance = savings_balance or 0
+        self.bills = bills or []
+        self.bill_reminders = bill_reminders or []
 
         # these need added to database push and pull
-        self.monthly_income = monthly_income
-        self.expenses = expenses
-        self.budgets = budgets
-        self.budget_alerts = []
-        self.current_debt = current_debt
-        self.total_debt = total_debt
-        self.assets = assets
-        self.credit_card_balances_and_limits = credit_card_balances_and_limits
-        self.net_worth = net_worth
+        self.monthly_income = monthly_income or 0
+        self.expenses = expenses or []
+        self.budgets = budgets or []
+        self.budget_alerts = budget_alerts or []
+        self.current_debt = current_debt or []
+        self.total_debt = total_debt or 0
+        self.assets = assets or 0
+        self.credit_card_balances_and_limits = credit_card_balances_and_limits or []
+        self.net_worth = net_worth or 0
 
     def get_account_details(self) -> dict:
         return {USER_ID: self._user_id, ACCOUNT_HOLDER_NAME: self.holder_name, USER_NAME: self.user_name,
@@ -38,7 +36,10 @@ class Account:
                 CHECKING_BALANCE: self.checking_balance,
                 SAVINGS_BALANCE: self.savings_balance,
                 CURRENT_BUDGET_WARNINGS: self.current_budget_warnings,
-                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders}
+                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders, MONTHLY_INCOME: self.monthly_income,
+                EXPENSES: self.expenses, BUDGETS: self.budgets, BUDGET_ALERTS: self.budget_alerts,
+                CURRENT_DEBT: self.current_debt, TOTAL_DEBT: self.total_debt, ASSETS: self.assets,
+                CREDIT_CARD_BALANCES_AND_LIMITS: self.credit_card_balances_and_limits, NETWORTH: self.net_worth}
 
     def get_new_account_details(self):
         """
@@ -66,4 +67,7 @@ class Account:
                 CHECKING_BALANCE: tran.balance[CHECKING_BALANCE],
                 SAVINGS_BALANCE: tran.balance[SAVINGS_BALANCE],
                 CURRENT_BUDGET_WARNINGS: self.current_budget_warnings,
-                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders}
+                BILLS: self.bills, BILL_REMINDERS: self.bill_reminders, MONTHLY_INCOME: self.monthly_income,
+                EXPENSES: self.expenses, BUDGETS: self.budgets, BUDGET_ALERTS: self.budget_alerts,
+                CURRENT_DEBT: self.current_debt, TOTAL_DEBT: self.total_debt, ASSETS: self.assets,
+                CREDIT_CARD_BALANCES_AND_LIMITS: self.credit_card_balances_and_limits, NETWORTH: self.net_worth}
